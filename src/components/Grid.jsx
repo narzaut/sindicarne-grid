a10e9a081ecb4498b592d125947f8eea3eb1aa25
narz <i.arzaut@itecriocuarto.org.ar>
2021-09-27 18:50:41 -0300

    deploy v1.0

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,13 +5,14 @@ import { getPostulantes } from '../helpers/getPostulantes'
 import { Postulante } from './Postulante'
 import { ModalPostulante } from './ModalPostulante'
 import { GlobalContext } from '../context/GlobalState'
+import { SearchBar } from "./SearchBar";
 
 export const Grid = () => {
 	const [loading, setLoading] = useState(true)
-	const [searchTerm, setSearchTerm] = useState('')
-	const { modalPostulanteState, postulantesState } = useContext(GlobalContext)
+	const { modalPostulanteState, postulantesState, filteredPostulantesState } = useContext(GlobalContext)
   const [modalPostulante, setModalPostulante] = modalPostulanteState;
 	const [postulantes, setPostulantes] = postulantesState
+	const [filteredPostulantes, setFilteredPostulantes] = filteredPostulantesState 
 
 	//CONSUMO API
 	useEffect(async () => {
@@ -28,40 +29,22 @@ export const Grid = () => {
 					<div className='h-80 flex flex-col items-center justify-center gap-2'>
 						<p className='uppercase text-semibold '>Cargando</p><div className=" lds-dual-ring-blue	text-shadow " />
 					</div>
-					:
+				:
 					modalPostulante.status == false ?
 						<div className='fadeIn fadeOutflex w-full text-gray-800 flex flex-col items-center justify-center '>
 							<div className=' pb-4 flex flex-col items-center justify-center'>
 								<p className=' max-w-min font-bold  uppercase border-b-2 border-green text-lg'>Postulantes</p>
 								<div className='w-3/4 pt-4'>
-									<input
-										value={searchTerm}
-										onChange={(e) => setSearchTerm(e.target.value)}
-										type='text'
-										onBlur={(e) => e.target.placeholder = 'Nombre / DNI'}
-										onSelect={(e) => e.target.placeholder = ''}
-										className='rounded border-2 py-2 border-gray-400 focus-border-green outline-none text-center w-full' placeholder='Nombre / DNI'
-									/>
+									<SearchBar data={ postulantes }/>
 								</div>
 							</div>
 							<div>
 								{typeof postulantes == 'object' ?
 									postulantes.length > 0 ? 
-										postulantes
-											.filter((postulante) => {
-												if (searchTerm == '') {
-													return postulante
-												} else if (postulante.nombrePostulante.toLowerCase().includes(searchTerm.toLowerCase())) {
-													return postulante
-												} else if (postulante.dniPostulante.toLowerCase().includes(searchTerm.toLowerCase())) {
-													return postulante
-												} else {
-													return false
-												}
-											})
-											.map(postulante => postulante ? <Postulante postulante={postulante} /> : <p>No se encuentra</p>)
+										filteredPostulantes								
+											.map(postulante => postulante ? <Postulante postulante={postulante} /> : '')
 									: 
-									<p className='text-center px-10'>No hay postulantes cargados en la base de datos.</p>
+										<p className='text-center px-10'>No hay postulantes cargados en la base de datos.</p>
 								:
 									<p className='text-center px-10'>Hubo un error. <br /> Intente nuevamente más tarde.</p>
 								}
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -6,8 +6,9 @@ export const GlobalContext = createContext();
 export const Provider = (props) => {
 	const [modalPostulante, setModalPostulante] = useState({ status: false, currentIndex: 0 })
 	const [postulantes, setPostulantes] = useState([])
+	const [filteredPostulantes, setFilteredPostulantes] = useState(postulantes)
 	return (
-		<GlobalContext.Provider value={{ postulantesState: [postulantes, setPostulantes], modalPostulanteState: [modalPostulante, setModalPostulante] }}>
+		<GlobalContext.Provider value={{ filteredPostulantesState: [filteredPostulantes, setFilteredPostulantes], postulantesState: [postulantes, setPostulantes], modalPostulanteState: [modalPostulante, setModalPostulante] }}>
 			{props.children}
 		</GlobalContext.Provider>
 	)
